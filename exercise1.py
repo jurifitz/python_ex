10e243473e260f6e424bbd095c73bfdabac98155
jurifitz <juri.fitz@students.unibe.ch>
2021-03-09 16:39:01 +0100

    expanded

--- exercise1.py
+++ exercise1.py
@@ -7,5 +7,9 @@ while a % 10 != 0:
     if a % 10 != 0:
         a = a + 1
         cntr = cntr + 1
+if age % 10 == 0:
+    print(f"Hello {name}, now you are {age} years old. This is a round year!")
+elif cntr == 1:
+    print(f"Hello {name}, now you are {age} years old. That means you'll be {a} next year")
 else:
     print(f"Hello {name}, now you are {age} years old. That means in {cntr} years you'll be {a}")
