420012d600434ca4bccd8a9a5dcd946ea8303cac
jurifitz <juri.fitz@students.unibe.ch>
2021-03-09 15:56:34 +0100

    guess number

---
+++ exercise2.py
@@ -0,0 +1,16 @@
+##copy code into python console
+
+import random
+
+r = random.randint(1,100)
+
+while x !=r :
+
+    x = int(input("Guess a number between 0 and 100. You can only exit, if you guess correctly! "))
+
+    if x < r:
+        print('higher')
+    elif x > r:
+        print('lower')
+    else:
+        print("correct, you won!")
