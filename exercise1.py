d0ca6338675d43f815c094bebee96269274e1e07
jurifitz <juri.fitz@students.unibe.ch>
2021-03-09 16:29:43 +0100

    new

--- exercise1.py
+++ exercise1.py
@@ -1,3 +1,11 @@
-a = ['Mary', 'had', 'a', 'little', 'lamb']
-for i in range(len(a)):
-    print(i, a[i])
+##copy code into python console
+name = str(input("Hello, please tell me your name "))
+age = int(input("and your age"))
+a = age
+cntr = 0
+while a % 10 != 0:
+    if a % 10 != 0:
+        a = a + 1
+        cntr = cntr + 1
+else:
+    print(f"Hello {name}, now you are {age} years old. That means in {cntr} years you'll be {a}")
